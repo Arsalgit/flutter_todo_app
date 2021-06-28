e8ab5006333712981a25552d6a17ed77a5507137
arsalsohail321 <arsalsohail373@gmail.com>
2021-06-28 23:57:36 +0500

    Flutter TodoApp

---
+++ lib/TodoApp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class TodoApp extends StatefulWidget {
+
+  @override
+  _TodoAppState createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
