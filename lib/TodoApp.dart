823957a22c567167dac0036e806f840add2e8c5a
arsalsohail321 <arsalsohail373@gmail.com>
2021-07-01 02:40:19 +0500

    .

--- lib/TodoApp.dart
+++ lib/TodoApp.dart
@@ -8,19 +8,58 @@ class TodoApp extends StatefulWidget {
 class _TodoAppState extends State<TodoApp> {
   @override
   Widget build(BuildContext context) {
+    var output = "";
     List<dynamic> lst = ["1", "2", "3"];
-    return Container(
-        color: Colors.black,
-        child: ListView.builder(
-            itemCount: lst.length,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.only(top: 10),
-                color: Colors.white,
-                child: ListTile(
-                  title: Text("${lst[index]}"),
-                ),
-              );
-            }));
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("ToDo App"),
+        ),
+        body: Container(
+            color: Colors.black,
+            child: ListView.builder(
+                itemCount: lst.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: EdgeInsets.only(top: 10),
+                    color: Colors.white,
+                    child: ListTile(
+                      title: Text("${lst[index]}"),
+                      trailing: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              lst.remove(index);
+                            });
+                          },
+                          child: Icon(Icons.delete)),
+                    ),
+                  );
+                })),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Add Item"),
+                    content: TextField(
+                      onChanged: (value) {
+                        output = value;
+                      },
+                    ),
+                    actions: [
+                      ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              lst.add(output);
+                            });
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Add"))
+                    ],
+                  );
+                });
+          },
+          child: Icon(Icons.add),
+        ));
   }
 }
