5a3088d332bab0910a893467ca7d8c87667d1203
arsalsohail321 <arsalsohail373@gmail.com>
2021-07-01 02:38:41 +0500

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_app/TodoApp.dart';
 
+import 'TodoApp.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,26 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text("ToDo App Example"),
-            ),
-            body: TodoApp(),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: Text("Add Item"),
-                        content: TextField(
-                          onChanged: (value) {},
-                        ),
-                      );
-                    });
-              },
-              child: Icon(Icons.add),
-            )));
+    return MaterialApp(home: TodoApp());
   }
 }
